6f06c4b2e42f6b5062adb71a445914003b177150
DuyDucIU <bdduc682@gmail.com>
2026-01-21 21:25:38 +0700

    Initial commit

---
+++ workout-tracker-backend/src/test/java/com/duyduc/workout_tracker/WorkoutTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duyduc.workout_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
